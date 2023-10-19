233e768526a7224e4e85597cbc3f26058cd06137
chickendining <sumethsiripakorn@gmail.com>
2023-10-19 23:05:47 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+  const [numPokemons, setNumPokemons] = useState(10);
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${numPokemons}`;
+
+  const handleShowPokemons = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    setPokemons(data.results);
+  }
 
   return (
     <>
+      <h1>Pokemon List</h1>
+      <label htmlFor="">
+        <input placeholder='Number of Pokemon' type="text" value={numPokemons} onChange={(e) => setNumPokemons(e.target.value)}/>
+      </label>
+      <button onClick={handleShowPokemons}>Show Pokemons</button>
+      <ul>
+        {pokemons.map((poke, index) => (
+          <li key={index}>
+            <h3>{poke.name}</h3>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
